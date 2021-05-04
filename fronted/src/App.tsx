a70f3d34dd54912004a8b67c1543f97ca5e2829f
unknown <hrkamizato@gmail.com>
2021-05-04 08:09:12 -0300

    static charts

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,4 +1,6 @@
+import Barchart from 'components/barchart';
 import Datatable from 'components/datatable';
+import Donutchart from 'components/donutchart';
 import Footer from 'components/footer';
 import Navbar from 'components/navbar';
 import React from 'react';
@@ -9,11 +11,24 @@ function App() {
       <Navbar />
       <div className="container">
 
-        <h1 className="text-primary">Olá mundo!</h1>
-        <Datatable/>
+        <h1 className="text-primary py-3">Dashboard de vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary">Taxa de Sucesso (%)</h5>
+            <Barchart />
+          </div>
+          <div className="col-sm-6">
+          <h5 className="text-center text-secundary">Todas as vendas</h5>
+            <Donutchart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas</h2>
+        </div>
+        <Datatable />
 
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
