99ab01c8a5031e43d22593ecc7889c382046b4c3
unknown <hrkamizato@gmail.com>
2021-05-07 20:10:03 -0300

    Pagination

--- backend/src/main/java/hrkamiza/xyz/dsvendas/services/SellerService.java
+++ backend/src/main/java/hrkamiza/xyz/dsvendas/services/SellerService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import hrkamiza.xyz.dsvendas.dto.SellerDTO;
 import hrkamiza.xyz.dsvendas.entities.Seller;
@@ -16,6 +17,7 @@ public class SellerService {
 	@Autowired
 	private SellerRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<SellerDTO> findAll(){
 	List<Seller> result = repository.findAll();
 	return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
